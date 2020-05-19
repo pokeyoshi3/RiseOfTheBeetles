5b6d44252da31bf82756b187d5ebe3bbeabf30ab
feli <killerkralle@gmail.com>
2020-05-19 15:05:07 +0200

    Ingame UI WIP

---
+++ Beetle Game/Assets/_FeliStuff/Scripts/HP_Petal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HP_Petal : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// Set the "falling" animation when this health point is lost
+    /// </summary>
+    public void Fall()
+    {
+        anim.SetBool("destroy", true);
+    }
+
+    /// <summary>
+    /// Set the "return" animation when this health point is regained
+    /// </summary>
+    public void Regain()
+    {
+        anim.SetBool("destroy", false);
+    }
+}
---
+++ Beetle Game/Assets/_FeliStuff/Scripts/UI_InGame.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Controls the player relevant ingame UI with health, abilites and resources
+/// </summary>
+public class UI_InGame : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Ref to the Health Petals; [0] = 1 life, [5] = 6 lives")]
+    private HP_Petal[] petalsForHP;
+    [SerializeField]
+    [Tooltip("Image for the ability UI")]
+    private Image abilityIconOne;
+    [SerializeField]
+    [Tooltip("Image for the ability UI")]
+    private Image abilityIconTwo;
+    [SerializeField]
+    private Text resOnPlayer;
+    [SerializeField]
+    private Text resInBase;
+
+    //HACK for testing purposes
+    int testMaxHP = 6;    
+    public void TestHP(bool lose)
+    {
+        if (lose)
+        {
+            if (testMaxHP == 0)
+                print("no HP left");
+            else
+            {
+                ChangeHP(testMaxHP, testMaxHP - 1);
+                testMaxHP -= 1;
+            }
+        }
+        else
+        {
+            if (testMaxHP == 6)
+                print("full hp reached");
+            else
+            {
+                ChangeHP(testMaxHP, testMaxHP + 1);
+                testMaxHP += 1;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Changes HP UI according to health loss/gain
+    /// Currently assumes that you lose only one petal per call
+    /// </summary>
+    private void ChangeHP(int prevHP, int newHP)
+    {      
+        //lost health
+        if (prevHP > newHP)
+        {
+            if (prevHP-newHP == 1)
+                petalsForHP[newHP].Fall();
+        }
+        //regained health
+        else if (prevHP < newHP)
+        {
+            int gained = newHP - prevHP;
+            for(int i = 0; i < gained; i++)
+            {
+                petalsForHP[prevHP+i].Regain();
+            }
+        }
+        //else: no hp change -> no ui change
+    }
+
+    private void ChangePlayerRessources(int newResCount)
+    {
+        resOnPlayer.text = newResCount.ToString();
+    }
+
+    private void ChangeBaseRessources(int newResCount)
+    {
+        resInBase.text = newResCount.ToString();
+    }
+
+    /// <summary>
+    /// Changes Ability Icon 1 if first is true, else changes Ability Icon 2
+    /// </summary>
+    /// <param name="first"></param>
+    private void SetAbilityIcons(Sprite abilityIcon, bool first)
+    {
+        if (first)
+            abilityIconOne.sprite = abilityIcon;
+        else
+            abilityIconTwo.sprite = abilityIcon;
+    }
+}
